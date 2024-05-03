ffa1eeec0efc36e298aa759ed320228c7ef3bb9b
ian-soares <ian.soaresss@gmail.com>
2024-05-03 12:08:24 -0300

    fix: removing aws region

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "budget_limit_amount" {
   description = "Budget limit amount"
   type        = number
